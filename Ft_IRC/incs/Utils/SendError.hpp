5e59097a2ac5c53312da0cc6da0cc4f88936d690
DoughnutsNCookies <chuahtseyung2002@gmail.com>
2023-11-28 18:20:41 +0800

    Added more errors in SendError

--- Ft_IRC/incs/Utils/SendError.hpp
+++ Ft_IRC/incs/Utils/SendError.hpp
@@ -6,42 +6,55 @@
 /*   By: schuah <schuah@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 17:22:31 by plau              #+#    #+#             */
-/*   Updated: 2023/11/28 12:26:34 by schuah           ###   ########.fr       */
+/*   Updated: 2023/11/28 18:06:33 by schuah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SENDERROR_HPP
 #define SENDERROR_HPP
 
+#include "Utils/irc.hpp"
 #include "Server/Client.hpp"
 
 class SendError {
 	public:
 		SendError();
+
+		/* ERR_NOSUCHNICK */
+		void				error401(t_irc& irc, Client& client, std::string nickname);
 		
+		/* ERR_NORECIPIENT */
+		void				error411(t_irc& irc, Client& client, std::string command);
+
+		/* ERR_NOTEXTTOSEND */
+		void				error412(t_irc& irc, Client& client);
+
 		/* ERR_UNKNOWNCOMMAND */
-		void	error421(Client& client, std::string unknownCommand);
+		void				error421(t_irc& irc, Client& client, std::string unknownCommand);
 
 		/* ERR_NONICKNAMEGIVEN */
-		void	error431(Client& client);
+		void				error431(t_irc& irc, Client& client);
 
 		/* ERR_ERRONEUSNICKNAME */
-		void	error432(Client& client);
+		void				error432(t_irc& irc, Client& client, std::string nickname);
 
 		/* ERR_NICKNAMEINUSE */
-		void	error433(Client& client);
+		void				error433(t_irc& irc, Client& client, std::string nickname);
 
 		/* ERR_NOTREGISTERED */
-		void	error451(Client& client);
+		void				error451(t_irc& irc, Client& client);
 
 		/* ERR_NEEDMOREPARAMS */
-		void	error461(Client& client);
+		void				error461(t_irc& irc, Client& client);
 
 		/* ERR_ALREADYREGISTRED */
-		void	error462(Client& client);
+		void				error462(t_irc& irc, Client& client);
 
 		/* ERR_PASSWDMISMATCH */
-		void	error464(Client& client);
+		void				error464(t_irc& irc, Client& client);
+	
+	private:
+		std::string	header(t_irc& irc, Client& client);
 };
 
 #endif
--- Ft_IRC/srcs/Utils/SendError.cpp
+++ Ft_IRC/srcs/Utils/SendError.cpp
@@ -6,7 +6,7 @@
 /*   By: schuah <schuah@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 17:26:43 by plau              #+#    #+#             */
-/*   Updated: 2023/11/28 12:25:00 by schuah           ###   ########.fr       */
+/*   Updated: 2023/11/28 18:10:32 by schuah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,50 @@
 
 SendError::SendError() {}
 
-void	SendError::error421(Client& client, std::string unknownCommand) {
-	client._response = "421 " +  client._nickname + " " + unknownCommand + " :Unknown command\r\n";
+void	SendError::error401(t_irc& irc, Client& client, std::string nickname) {
+	client.response = this->header(irc, client) + " 401 " +  client.nickname + " " + nickname + " :No such nick/channel\r\n";
 }
 
-void	SendError::error431(Client& client) {
-	client._response = "431 " +  client._nickname + " :No nickname given\r\n";
+void	SendError::error411(t_irc& irc, Client& client, std::string command) {
+	client.response = this->header(irc, client) + " 411 " +  client.nickname + " :No recipient given (" + command + ")\r\n";
 }
 
-void	SendError::error432(Client& client) {
-	client._response = "432 " +  client._nickname + " " + client._nickname + " :Erroneus nickname\r\n";
+void	SendError::error412(t_irc& irc, Client& client) {
+	client.response = this->header(irc, client) + " 412 " +  client.nickname + " :No text to send\r\n";
 }
 
-void	SendError::error433(Client& client) {
-	client._response = "433 " +  client._nickname + " " + client._nickname + " :Nickname is already in use\r\n";
+void	SendError::error421(t_irc& irc, Client& client, std::string unknownCommand) {
+	client.response = this->header(irc, client) + " 421 " +  client.nickname + " " + unknownCommand + " :Unknown command\r\n";
 }
 
-void	SendError::error451(Client& client) {
-	client._response = "451 " +  client._nickname + " :You have not registered\r\n";
+void	SendError::error431(t_irc& irc, Client& client) {
+	client.response = this->header(irc, client) + " 431 " +  client.nickname + " :No nickname given\r\n";
 }
 
-void	SendError::error461(Client& client) {
-	client._response = "461 " +  client._nickname + " PASS :Not enough parameters\r\n";
+void	SendError::error432(t_irc& irc, Client& client, std::string nickname) {
+	client.response = this->header(irc, client) + " 432 " +  client.nickname + " " + nickname + " :Erroneus nickname\r\n";
 }
 
-void	SendError::error462(Client& client) {
-	client._response = "462 " +  client._nickname + " :You may not reregister\r\n";
+void	SendError::error433(t_irc& irc, Client& client, std::string nickname) {
+	client.response = this->header(irc, client) + " 433 " +  client.nickname + " " + nickname + " :Nickname is already in use\r\n";
 }
 
-void	SendError::error464(Client& client) {
-	client._response = "464 " +  client._nickname + " :Password incorrect\r\n";
+void	SendError::error451(t_irc& irc, Client& client) {
+	client.response = this->header(irc, client) + " 451 " +  client.nickname + " :You have not registered\r\n";
+}
+
+void	SendError::error461(t_irc& irc, Client& client) {
+	client.response = this->header(irc, client) + " 461 " +  client.nickname + " PASS :Not enough parameters\r\n";
+}
+
+void	SendError::error462(t_irc& irc, Client& client) {
+	client.response = this->header(irc, client) + " 462 " +  client.nickname + " :You may not reregister\r\n";
+}
+
+void	SendError::error464(t_irc& irc, Client& client) {
+	client.response = this->header(irc, client) + " 464 " +  client.nickname + " :Password incorrect\r\n";
+}
+
+std::string	SendError::header(t_irc& irc, Client& client) {
+	return (":" + irc.hostname + ":" + std::to_string(irc.port) + " " + client.nickname + " ");
 }
