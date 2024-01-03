3c9719003c11c5b0ad6a6e2e87cc5ee8f1fb7a0b
DoughnutsNCookies <chuahtseyung2002@gmail.com>
2024-01-03 21:16:24 +0800

    Refractor User

--- Ft_IRC/srcs/Commands/User.cpp
+++ Ft_IRC/srcs/Commands/User.cpp
@@ -6,7 +6,7 @@
 /*   By: schuah <schuah@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 13:33:41 by plau              #+#    #+#             */
-/*   Updated: 2024/01/03 21:01:36 by schuah           ###   ########.fr       */
+/*   Updated: 2024/01/03 21:15:40 by schuah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,18 @@ void	User::verifyTokens(t_irc& irc, Client& client, tokensVector &tokens) {
 		this->_SendMsg.error462(irc, client);
 		return;
 	}
+
 	if (tokens[2] != "0" || tokens[3] != "*") {
 		this->_SendMsg.error461(irc, client, tokens[0]);
 		return;
 	}
-	this->_parseTokens(tokens);
-	if (this->_username.size() == 0) {
+
+	if (tokens.size() < 5) {
 		this->_SendMsg.error461(irc, client, tokens[0]);
 		return;
 	}
+
+	this->_parseTokens(tokens);
 	this->_executeCommand(irc, client);
 }
 
@@ -37,10 +40,7 @@ void	User::_parseTokens(tokensVector &tokens) {
 		this->_username = this->_username.substr(0, 15);
 	
 	this->_realname = tokens[4];
-	size_t	tokensSize = tokens.size();
-	if (tokensSize < 5)
-		this->_realname = "";
-	for (size_t i = 5; i < tokensSize; i++)
+	for (size_t i = 5; i < tokens.size(); i++)
 		this->_realname += " " + tokens[i];
 	this->_realname = this->_Utils.extractFromToken(this->_realname);
 }
@@ -49,6 +49,4 @@ void::User::_executeCommand(t_irc& irc, Client& client) {
 	(void)irc;
 	client.username = this->_username;
 	client.realname = this->_realname;
-	if (client.realname.empty())
-		client.realname = client.nickname;
 }
