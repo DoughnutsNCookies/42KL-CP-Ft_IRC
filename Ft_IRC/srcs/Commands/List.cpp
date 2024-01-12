b7c1b5e69982df67942107d1814074e4033979b4
DoughnutsNCookies <chuahtseyung2002@gmail.com>
2024-01-12 16:29:21 +0800

    Fix list crash

--- Ft_IRC/srcs/Commands/List.cpp
+++ Ft_IRC/srcs/Commands/List.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   List.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: plau <plau@student.42.kl>                  +#+  +:+       +#+        */
+/*   By: schuah <schuah@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 16:24:13 by plau              #+#    #+#             */
-/*   Updated: 2024/01/11 22:03:34 by plau             ###   ########.fr       */
+/*   Updated: 2024/01/12 16:28:43 by schuah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,14 +43,17 @@ void	List::_executeCommand(t_irc& irc, Client& client) {
 }
 
 void	List::_displayAllLists(t_irc& irc, Client& client) {
-	for (std::map<std::string, Channel>::iterator it = irc.channels.begin(); it != irc.channels.end(); it++) {
+	for (std::map<std::string, Channel>::iterator it = irc.channels.begin(); it != irc.channels.end(); it++)
 		this->_SendMsg.rpl322(irc, client, it->second);
-	}
 }
 
 void	List::_displaySpecifiedLists(t_irc& irc, Client& client) {
 	for (size_t i = 0; i < this->_channelList.size(); i++) {
-		Channel&	channel = this->_Utils.getChannelByName(irc, this->_channelList[i]);
-		this->_SendMsg.rpl322(irc, client, channel);
+		try {
+			Channel&	channel = this->_Utils.getChannelByName(irc, this->_channelList[i]);
+			this->_SendMsg.rpl322(irc, client, channel);
+		} catch (Utils::NoChannelFoundException e) {
+			continue;
+		}
 	}
 }
