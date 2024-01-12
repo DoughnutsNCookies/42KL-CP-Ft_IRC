282995c2b74c4f63d7d1bd0012c07b2bcb65c29d
nuyiep <nuyiep@hotmail.com>
2024-01-12 19:16:21 +0800

    Kick fixed

--- Ft_IRC/srcs/Commands/Kick.cpp
+++ Ft_IRC/srcs/Commands/Kick.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Kick.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: schuah <schuah@student.42.fr>              +#+  +:+       +#+        */
+/*   By: plau <plau@student.42.kl>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 16:43:26 by schuah            #+#    #+#             */
-/*   Updated: 2024/01/12 19:03:17 by schuah           ###   ########.fr       */
+/*   Updated: 2024/01/12 19:16:00 by plau             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,15 +56,11 @@ void	Kick::verifyTokens(t_irc& irc, Client& client, tokensVector& tokens) {
 
 void	Kick::_parseTokens(tokensVector& tokens) {
 	this->_channelName = this->_Utils.extractFromToken(tokens[1]);
-	
+
 	std::string	nicknames = this->_Utils.extractFromToken(tokens[2]);
 	this->_nicknames = this->_Parser.parse(nicknames, ",", false);
 	if (this->_nicknames.size() == 0)
 		return;
-	for (size_t i = 0; i < this->_nicknames.size(); i++) {
-		if (this->_nicknames[i][0] != '#')
-			this->_nicknames[i] = "#" + this->_nicknames[i];
-	}
 
 	this->_comment = "No commment given";
 	if (tokens.size() > 3)
@@ -75,16 +71,16 @@ void	Kick::_parseTokens(tokensVector& tokens) {
 
 void	Kick::_executeCommand(t_irc& irc, Client& client) {
 	(void)client;
-	Channel&	channel = irc.channels[this->_channelName];
+	Channel&	channel = this->_Utils.getChannelByName(irc, this->_channelName);
 
 	for (size_t i = 0; i < this->_nicknames.size(); i++) {
 		if (channel.users.find(this->_nicknames[i]) == channel.users.end())
 			continue;
+		std::string	message = ":" + client.nickname + " KICK " + this->_channelName + " " + this->_nicknames[i] + " :" + this->_comment + "\r\n";
+		this->_SendMsg.sendToAllUsersInChannel(irc, client, channel, message, true);
+
 		channel.users.erase(this->_nicknames[i]);
 		Client& user = this->_Utils.getClientByNickname(irc, this->_nicknames[i]);
 		user.channels.erase(std::find(user.channels.begin(), user.channels.end(), this->_channelName));
-		
-		std::string	message = ":" + client.nickname + " KICK " + this->_channelName + " " + this->_nicknames[i] + " :" + this->_comment + "\r\n";
-		this->_SendMsg.sendToAllUsersInChannel(irc, client, channel, message, true);
 	}
 }
