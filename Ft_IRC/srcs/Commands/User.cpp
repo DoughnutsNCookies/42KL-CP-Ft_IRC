5797f26da801bf705a5318f3bbb4db34174b4729
DoughnutsNCookies <chuahtseyung2002@gmail.com>
2024-01-12 17:23:40 +0800

    Fix username and realname can be empty

--- Ft_IRC/srcs/Commands/User.cpp
+++ Ft_IRC/srcs/Commands/User.cpp
@@ -6,7 +6,7 @@
 /*   By: schuah <schuah@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 13:33:41 by plau              #+#    #+#             */
-/*   Updated: 2024/01/11 21:45:15 by schuah           ###   ########.fr       */
+/*   Updated: 2024/01/12 17:23:30 by schuah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,14 +26,16 @@ void	User::verifyTokens(t_irc& irc, Client& client, tokensVector &tokens) {
 	}
 
 	this->_parseTokens(tokens);
+	if (this->_username.size() == 0 || this->_realname.size() == 0) {
+		this->_SendMsg.error461(irc, client, tokens[0]);
+		return;
+	}
+
 	this->_executeCommand(irc, client);
 }
 
 void	User::_parseTokens(tokensVector &tokens) {
 	this->_username = this->_Utils.extractFromToken(tokens[1]);
-	if (this->_username.size() > 15)
-		this->_username = this->_username.substr(0, 15);
-	
 	this->_realname = this->_Utils.extractFromToken(tokens[4]);
 	for (size_t i = 5; i < tokens.size(); i++)
 		this->_realname += " " + tokens[i];
